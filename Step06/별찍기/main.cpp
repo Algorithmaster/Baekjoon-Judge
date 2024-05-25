2887e7c299c084c3b5ac93ce7b898569519bb50a
Asher <machine.ksh@gmail.com>
2024-05-25 14:49:31 +0900

    pass_별찍기

--- Step06/별찍기/main.cpp
+++ Step06/별찍기/main.cpp
@@ -2,9 +2,37 @@
 
 int main()
 {
-    const char* cpHello = "Hello World!";
+    int nNumber = 0;
+    int nStep   = 1;
+    int nSqure  = 1;
+    bool nFlag  = false;
 
-    std::cout << cpHello;
+    std::cin >> nNumber;
+
+    for(int i=0; i<nNumber*2-1; i++)
+    {
+        for(int j=nNumber-nStep; j>0; j--)
+            std::cout << " ";
+        
+        for(int k=0; k < nSqure; k++)
+            std::cout << "*";
+
+        if(i >= nNumber-1)
+        {
+            nFlag = true;
+            nStep--;
+            nSqure -= 2;
+        }
+        else
+        {
+            nFlag = false;
+            nStep++;
+            nSqure += 2;
+        }
+
+        std::cout << "\n";
+    }
+
 
     return 0;
 }
