9abb622a7d1f7ff1317a680593a5c7481610852f
Asher <machine.ksh@gmail.com>
2024-05-25 15:07:27 +0900

    pass_팰린드롬

--- Step06/팰린드롬/main.cpp
+++ Step06/팰린드롬/main.cpp
@@ -1,6 +1,26 @@
 #include <iostream>
+#include <string>
 
 int main()
 {
+    std::string strPalindrome;
+
+    std::cin >> strPalindrome;
+
+    bool bSuccess = true;
+    for(int i=0; i<strPalindrome.length()/2; i++)
+    {
+        char a1 = strPalindrome[i];
+        char a2 = strPalindrome[strPalindrome.length()-1-i];
+
+        if(a1 != a2)
+            bSuccess = false;
+    }   
+
+    if(bSuccess)
+        std::cout << "1";
+    else
+        std::cout << "0";
+
     return 0;
 }
