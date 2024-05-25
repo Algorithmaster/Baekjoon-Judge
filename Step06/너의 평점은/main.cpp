25e1a16f82864b7be78adc533e4d2bc5f4f8353b
Asher <machine.ksh@gmail.com>
2024-05-25 16:22:41 +0900

    pass_너의 평점은

--- Step06/너의 평점은/main.cpp
+++ Step06/너의 평점은/main.cpp
@@ -1,7 +1,33 @@
 #include <iostream>
 
+using namespace std;
+
 int main()
 {
-    //test 0101
+    string strMajorName; double dGrade[20] = {0.0, }; string strInput;
+    double dAvg = 0.0; float sumGrade = 0.0;
+    double dScore[20] = {0.0, };
+
+    for(int i=0; i<20; i++)
+    {
+        cin >> strMajorName >> dGrade[i] >> strInput;
+
+        if("A+" == strInput)        dScore[i] = 4.5;
+        else if("A0" == strInput)   dScore[i] = 4.0;   
+        else if("B+" == strInput)   dScore[i] = 3.5;  
+        else if("B0" == strInput)   dScore[i] = 3.0;  
+        else if("C+" == strInput)   dScore[i] = 2.5;  
+        else if("C0" == strInput)   dScore[i] = 2.0;  
+        else if("D+" == strInput)   dScore[i] = 1.5;   
+        else if("D0" == strInput)   dScore[i] = 1.0;   
+        else if("F"  == strInput)   dScore[i] = 0.0;   
+        else if("P"  == strInput)   { dScore[i] = 0.0; dGrade[i] = 0.0;};
+
+        dAvg += dScore[i] * dGrade[i];
+        sumGrade += dGrade[i];
+    }
+
+    cout << dAvg/sumGrade;
+
     return 0;
 }
