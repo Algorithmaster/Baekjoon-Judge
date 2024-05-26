e60c10374f06c5e4bd5d3d992ed1a81b77f1b0fa
Algorithmaster <show5675@naver.com>
2024-05-26 17:13:55 +0900

    pass_step10_03_네_번째_점

---
+++ Step10/03. 네 번째 점/main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+int main()
+{
+    int x1, y1, x2, y2, x3, y3 = 0;
+
+    std::cin >> x1 >> y1 >> x2 >> y2 >> x3 >> y3;
+
+    std::cout << (x1^x2^x3) << " " << (y1^y2^y3);
+
+    return 0;
+}
